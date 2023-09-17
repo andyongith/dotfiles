9a9ee3bf9f99122b3b07f3d5ebfd2e0aa35c72b6
Andy <andyongit@gmail.com>
2023-09-17 12:59:36 +0530

    Fixed an issue in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+# creating some directories in case if they don't exist already
+mkdir -p $HOME/.config/hypr/ $HOME/.config/kitty/ $HOME/.config/waybar
+
+# for home directory
 ln -si $PWD/.bashrc $PWD/.gitconfig $PWD/.vimrc $HOME/
-ln -fsi $PWD/hypr $PWD/kitty $PWD/waybar $HOME/.config
+
+# hyprland
+ln -si $PWD/hypr/hyprland.conf $HOME/.config/hypr/
+
+# kitty
+ln -si $PWD/kitty/kitty.conf $HOME/.config/kitty/
+
+# waybar
+ln -si $PWD/waybar/config.jsonc $PWD/waybar/modules.jsonc $PWD/waybar/style.css $HOME/.config/waybar
